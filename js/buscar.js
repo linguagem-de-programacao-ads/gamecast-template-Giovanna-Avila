5d3343aadfcd2234e39a225d0da05ba0b415932e
Giovanna Avila <avilagiovanna21@gmail.com>
2024-04-21 18:54:17 -0300

    atualizacaoTerceiraAula

--- js/buscar.js
+++ js/buscar.js
@@ -1,17 +1,41 @@
-async function buscar (){
-    console.log("Passei por aqui")
+async function buscar(){
+    //localhost url da api rodando
+const resposta = await fetch("https://660f44b0356b87a55c510da3.mockapi.io/agendas");
+const respostaDadosAgenda = await resposta.json()
 
-    const resposta = await fetch("https://660f44ac356b87a55c510d09.mockapi.io/agendas");
-    const respostaDadosAgenda = await resposta.json();
+console.log("Resposta", respostaDadosAgenda)
 
-    console.log("Resposta: ", respostaDadosAgenda);
-    
-    const cards = document.getElementById("cards_games");
 
-    cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
-          return  "TESTE"
-    });
+const cards = document.getElementById("cards_games")
+
+cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
+return `
+<div class="cardItem">
+<div class="dataGame"> 
+<img src="../imagens/calendar-solid.svg" alt=""> 20/04 21:30 
+</div>
+<img class="img-background" src="${itemAgenda.urlImagem}" alt="">
+<div class="descricao">
+<p> <strong>${itemAgenda.nome}</strong> </p>
+<p> ${itemAgenda.descricao} </p>
+<p><strong>Gamers: </strong></p>
+
+<div class="gamers">
+${itemAgenda.gamers.map((gamerItem)=> {
+return `<div class="gamerItem">@${gamerItem}</div>`
+}).join('')
 }
-console.log("Antes de buscar")
-buscar()
-console.log("Depois de buscar")
+</div>
+
+
+</div>  
+<div class="assistir">
+<a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+</div>
+</div>
+`
+}).join('')
+
+}
+
+buscar();
